dc095502fac55119a4b18b2d9514f8d81632015a
PDKHOA_K24CNT1 <Khoapham206.vn@gmail.com>
2026-09-26 01:06:47 +0700

    PdkLesson10EFDbFirst

---
+++ PdkLesson10EFDbFirst/Models/PdkLesson10EfdbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PdkLesson10EFDbFirst.Models;
+
+public partial class PdkLesson10EfdbContext : DbContext
+{
+    public PdkLesson10EfdbContext()
+    {
+    }
+
+    public PdkLesson10EfdbContext(DbContextOptions<PdkLesson10EfdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<PdkMember> PdkMembers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=KHOAPHAM\\SQLEXPRESS;Database=PdkLesson10EFDb;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PdkMember>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("PdkMember");
+
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.PdkEmail)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.PdkFullName).HasMaxLength(50);
+            entity.Property(e => e.PdkPassword)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.PdkPhone)
+                .HasMaxLength(12)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.PdkUserName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PdkLesson10EFDbFirst/Models/PdkMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PdkLesson10EFDbFirst.Models;
+
+public partial class PdkMember
+{
+    public long Id { get; set; }
+
+    public string? PdkUserName { get; set; }
+
+    public string? PdkPassword { get; set; }
+
+    public string? PdkFullName { get; set; }
+
+    public string? PdkEmail { get; set; }
+
+    public string? PdkPhone { get; set; }
+
+    public bool? PdkStatus { get; set; }
+}
